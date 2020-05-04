5eb2d0b9a818d746970536a26ee85ca2952e94a0
qwertpi <qwertpi@users.noreply.github.com>
2020-05-04 16:26:58 +0100

    Fixed typos caused by me typing way too fast, I promise I'm not actually that illiterate

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # tcp-simulator
-I was trying to get my head around how TCP works so I wrote some pseudocode and then I relalised I might aswell write some actual code that I can run to check if I am correct
+I was trying to get my head around how TCP works so I wrote some pseudo-code and then I realized I might as well write some actual code that I can run to check if I am correct
 ## Copyright
 Copyright © 2019  Rory Sharp All rights reserved.
 
@@ -11,17 +11,17 @@ the Free Software Foundation, either version 3 of the License, or
 You should have received a copy of the GNU General Public License
 along with this program.  If you have not received this, see <http://www.gnu.org/licenses/gpl-3.0.html>.
 
-For a summary of the licence go to https://tldrlegal.com/license/gnu-general-public-license-v3-(gpl-3)
+For a summary of the license go to https://tldrlegal.com/license/gnu-general-public-license-v3-(gpl-3)
 ## Installation
-### Prerequites
+### Prerequisites
 * Scala
 * Scalac (optional)
 ## Process
 0\. Download this repo (`git clone https://github.com/qwertpi/tcp-simulator.git`)  
 1\. Either run as a script (`scala DESIREDPROGRAM.scala`) or compile (`scalac DESIREDPROGRAM.scala`) then run the compiled code (`scalac DESIREDPROGRAM`)
 ## Basic.scala
-This is the simple simulation which I originaly wrote, simply press enter to advance the code when necacry
+This is the simple simulation which I originaly wrote, simply press enter to advance the code when neccacry
 To run this either do `scala basic.scala` or `scalac basic.scala && scala basictcp`
 ## A.scala and B.scala
 COMING SOON!  
-If I ever get around to writing this, this will be two seperate programs which can be ran as two seprarte process on either the same computer or two different computers. This will probably stick all the TCP transimitions in a text file in a location where it can be read by both programs
+If I ever get around to writing this, this will be two separate programs which can be ran as two separate process on either the same computer or two different computers. This will probably stick all the TCP transmissions in a text file in a location where it can be read by both programs
--- basic.scala
+++ basic.scala
@@ -6,28 +6,28 @@ import math.pow
 object basictcp extends App {
     println("Starting TCP Handshake")
     println("Computer A is generating a random sequence number  ")
-    // sqaures then square roots to ensure it is positive
+    // squares then square roots to ensure it is positive
     var a_seq: Int = sqrt(pow(Random.nextInt(), 2)).toInt
     
     println(s"A sends SYN, seq=$a_seq, len=0")
     println("")
     readLine()
     
-    println("Computer B recivies the packet from A and will increment the recived seuqeucne number by one as part of the ACK process")
-    println("Computer B also needs to pick it's own random seuqence number and ensure this reaches A")
-    // sqaures then square roots to ensure it is positive
+    println("Computer B receives the packet from A and will increment the received sequence number by one as part of the ACK process")
+    println("Computer B also needs to pick it's own random sequence number and ensure this reaches A")
+    // squares then square roots to ensure it is positive
     var b_seq: Int = sqrt(pow(Random.nextInt(), 2)).toInt
     a_seq += 1
     println(s"B sends SYN/ACK, seq=$b_seq, ack=$a_seq, len=0")
     println("")
     readLine()
     
-    println("Computer A recives B's ACK and B's sequence number")
+    println("Computer A receives B's ACK and B's sequence number")
     b_seq += 1
-    println("Computer A will now incrmeent and ACK B's sequence number")
+    println("Computer A will now increment and ACK B's sequence number")
     println(s"A sends SYN/ACK, seq=$a_seq, ack=$b_seq, len=0")
     
-    println("TCP handshake complete, both devices are now standing by for data transmition")
+    println("TCP handshake complete, both devices are now standing by for data transmission")
     var data_size: Int = 0
     var not_done: Boolean = true
     while (not_done) {
@@ -42,13 +42,13 @@ object basictcp extends App {
             println("")
             readLine()
             
-            println("B measures the size of the data it recives and increments its ACK acordingly")
+            println("B measures the size of the data it receives and increments its ACK accordingly")
             b_seq += data_size
             println(s"A sends ACK, seq=$a_seq, ack=$b_seq, len=0")
         }
         
     }
-    println("TCP teardown")
+    println("TCP tear-down")
     println(s"A sends FIN, seq=$a_seq, ack=$b_seq")
     println("")
     readLine()
